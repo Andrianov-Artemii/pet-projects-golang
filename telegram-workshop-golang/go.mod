fb7bea2d6f87f7777f84bf577216d8049094f5c6
Andrianov-Artemii <andrianovartemii@gmail.com>
2021-09-23 12:36:10 +0500

    Added telegram-bot workshop template

---
+++ telegram-workshop-golang/go.mod
@@ -0,0 +1,13 @@
+module mod
+
+go 1.17
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf
+)
+
+require (
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ telegram-workshop-golang/models.go
@@ -0,0 +1,31 @@
+// Объявление пакета main
+package main
+
+// Модели для парсинга данных из XML Центробанка
+
+// TODO: создать структуру для парсинга данных
+
+
+/*
+
+Пример пришедшего XML-ответа
+
+<?xml version="1.0" encoding="windows-1251"?>
+<ValCurs Date="02.03.2002" name="Foreign Currency Market">
+    <Valute ID="R01010">
+        <NumCode>036</NumCode>
+        <CharCode>AUD</CharCode>
+        <Nominal>1</Nominal>
+        <Name>Наименование валюты</Name>
+        <Value>16,0102</Value>
+    </Valute>
+    <Valute ID="R01035">
+        <NumCode>826</NumCode>
+        <CharCode>GBP</CharCode>
+        <Nominal>1</Nominal>
+        <Name>Наименование валюты</Name>
+        <Value>43,8254</Value>
+    </Valute>
+</ValCurs>
+
+*/
